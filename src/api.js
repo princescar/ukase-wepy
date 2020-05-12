5369a7e192b3238e95c669e866c807e4fd8452a4
雷欢 <huan.lei@downtown8.com>
2020-05-12 23:52:38 +0800

    暂时先加上预定姓名

--- src/api.js
+++ src/api.js
@@ -43,7 +43,8 @@ export default {
       itemCode,
       itemChargeCode,
       reserveTime,
-      reservePhone
+      reservePhone,
+      reserveName: '张三'
     })
   },
   async getBookings() {
