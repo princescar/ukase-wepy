c62832132bf6d81c6491962fc0656ebd9d3185d1
Halbert Lei <princescar@gmail.com>
2020-07-21 22:07:53 +0800

    improve scan logic.

--- src/services/scan.js
+++ src/services/scan.js
@@ -14,7 +14,7 @@ export default {
       await _handle()
     } catch (e) {
       console.error(e)
-      !init && wepy.showToast({ title: e.message, icon: 'none' })
+      !init && wepy.showModal({ title: '错误', content: e.message, showCancel: false })
       return false
     }
 
@@ -24,10 +24,13 @@ export default {
       const prefix = NORMAL_QRCODE_PREFIXS.find(x => content.startsWith(x))
       const [action, gymCode, codeNo] = content.substring(prefix.length).split('/')
 
-      if (action !== 'enter' && action !== 'leave' || !gymCode) throw new Error('不识别的二维码')
-      if (action === 'leave' && !codeNo) throw new Error('不识别的二维码')
+      if ((action !== 'enter' && action !== 'leave') || !gymCode) throw new Error('无法识别的二维码')
+      if (action === 'leave' && !codeNo) throw new Error('无法识别的二维码')
 
       const { canEnterInto } = await API.preEntranceCheck(gymCode)
+      if (canEnterInto && action === 'leave') throw new Error('无法出场，您不在场内')
+      if (!canEnterInto && action === 'enter') throw new Error('无法入场，您已在场内')
+
       const url = canEnterInto ? `/pages/enter?id=${gymCode}` : `/pages/leave?id=${gymCode}&code=${codeNo}`
       init ? wepy.redirectTo({ url }) : wepy.navigateTo({ url })
     }
